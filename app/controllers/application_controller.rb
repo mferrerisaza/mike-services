e2d48d71e1622e1975ba13b89ff5aa1742acb074
Miguel Ferrer <miguef7@gmail.com>
2018-09-20 01:04:27 -0500

    ajaxify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :set_user
+
+  private
+
+  def set_user
+    cookies[:username] = 'guest'
+    cookies[:username] = current_user.email unless current_user.blank?
+  end
 end
