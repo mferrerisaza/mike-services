0e04b5098acdafe9d87bed53d0685d0fa14b2343
Miguel Ferrer <miguef7@gmail.com>
2019-10-19 23:12:15 -0500

    more cleaning

--- app/channels/task_channel.rb
+++ app/channels/task_channel.rb
@@ -6,21 +6,4 @@ class TaskChannel < ApplicationCable::Channel
   def unsubscribed
     stop_all_streams
   end
-
-  def send_push_notification(data)
-    Webpush.payload_send(
-      message: data["message"],
-      endpoint: data["subscription"]["endpoint"],
-      p256dh: data["subscription"]["keys"]["p256dh"],
-      auth: data["subscription"]["keys"]["auth"],
-      vapid: {
-        subject: "mailto:sender@example.com",
-        public_key: ENV['VAPID_PUBLIC_KEY'],
-        private_key: ENV['VAPID_PRIVATE_KEY']
-      },
-      ssl_timeout: 5,
-      open_timeout: 5,
-      read_timeout: 5
-    )
-  end
 end
